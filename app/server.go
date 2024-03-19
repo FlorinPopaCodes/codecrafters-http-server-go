a4c9c69c976ea286ad7d0b0783e93789902f0073
Florin Popa <456185+FlorinPopaCodes@users.noreply.github.com>
2024-03-19 21:07:19 +0200

    Add files GET

--- app/server.go
+++ app/server.go
@@ -11,6 +11,13 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
 
+	// if it exists save the '--directory' flag value to the 'dir' variable
+	dir := ""
+	if len(os.Args) > 1 && os.Args[1] == "--directory" {
+		dir = os.Args[2]
+		println(dir)
+	}
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -38,7 +45,7 @@ func main() {
 			}
 			requestLines := strings.Split(string(buf), "\r\n")
 			splitFirstLine := strings.Split(requestLines[0], " ")
-			path := splitFirstLine[1]
+			method, path := splitFirstLine[0], splitFirstLine[1]
 
 			headers := make(map[string]string)
 			// parse headers
@@ -62,6 +69,39 @@ func main() {
 			} else if path == "/user-agent" {
 				response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s\r\n\r\n", len(headers["User-Agent"]), headers["User-Agent"])
 				c.Write([]byte(response))
+			} else if strings.HasPrefix(path, "/files") {
+				filePath := strings.Split(path, "/files/")[1]
+
+				if method == "GET" {
+					file, err := os.Open(dir + "/" + filePath)
+					if err != nil {
+						c.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+						return
+					}
+					defer file.Close()
+
+					fileInfo, err := file.Stat()
+					if err != nil {
+						c.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+						return
+					}
+
+					fileSize := fileInfo.Size()
+					fileContent := make([]byte, fileSize)
+					_, err = file.Read(fileContent)
+					if err != nil {
+						c.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+						return
+					}
+
+					response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s\r\n\r\n", fileSize, fileContent)
+					c.Write([]byte(response))
+				} else if method == "POST" {
+
+				} else {
+					c.Write([]byte("HTTP/1.1 405 Method Not Allowed\r\n\r\n"))
+				}
+
 			} else {
 				c.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 			}
