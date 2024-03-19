eede3ad89d18037b14c3c4a86d36bc6b552caec0
Florin Popa <456185+FlorinPopaCodes@users.noreply.github.com>
2024-03-19 12:53:41 +0200

    Testing response

--- app/server.go
+++ app/server.go
@@ -8,9 +8,7 @@ import (
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
+	// fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -18,9 +16,21 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+	for {
+		// Wait for a connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle the connection in a new goroutine.
+		// The loop then returns to accepting, so that
+		// multiple connections may be served concurrently.
+		go func(c net.Conn) {
+			c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+			// Shut down the connection.
+			c.Close()
+		}(conn)
 	}
 }
